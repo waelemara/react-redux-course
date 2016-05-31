42c7b7c4c5fa2c465c09b33619653b150a219244
Wael Emara <wael.s.emara@gmail.com>
2016-06-01 00:00:14 +1000

    ger redux to work with react

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
 
 render(
-    <Router history={browserHistory} routes={routes} />, document.getElementById('app')
+    <Provider store={store} >
+        <Router
+            history={browserHistory}
+            routes={routes} />
+    </Provider>,
+    document.getElementById('app')
 );
